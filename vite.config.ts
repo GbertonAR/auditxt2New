efbc543feee8cb12ffd59b8c03b4e6b7166f6c34
Gustavo Berton <tu_correo@ejemplo.com>
2025-06-22 21:51:25 -0300

    Testeo

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-// Obtener __dirname en ESM
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
-  // ✅ Cargar variables de entorno
-  const env = loadEnv(mode, process.cwd())
+  const env = loadEnv(mode, process.cwd(), '')
 
+  console.log('Modo:', mode)
+  console.log('Variable VITE_API_URL:', env.VITE_API_URL)
+
+  
   return {
     plugins: [react()],
     resolve: {
@@ -21,7 +19,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_API_BASE_URL || 'http://localhost:8000',
+          target: env.VITE_API_URL || 'http://localhost:8000',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
